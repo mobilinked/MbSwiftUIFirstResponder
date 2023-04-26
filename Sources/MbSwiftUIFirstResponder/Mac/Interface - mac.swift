1207f7c1833bbd3c44f69ee41946654856228087
Denys Telezhkin <strangervir@gmail.com>
2023-04-26 14:55:46 +0300

    Try out if simpy adding extension to SecureField actually works.

--- Sources/MbSwiftUIFirstResponder/Mac/Interface - mac.swift
+++ Sources/MbSwiftUIFirstResponder/Mac/Interface - mac.swift
@@ -14,6 +14,13 @@ extension TextField {
     }
 }
 
+extension SecureField {
+    public func firstResponder<V: Hashable>(id: V, firstResponder: Binding<V?>, resignableUserOperations: MbFirstResponder.TextField.ResignableUserOperations = .all, didBackViewLoaded: ((NSTextField) -> Void)? = nil) -> some View {
+        self
+            .background(MbFRHackView<V, NSTextField>(id: id, firstResponder: firstResponder, resignableUserOperations: resignableUserOperations, didBackViewLoaded: didBackViewLoaded))
+    }
+}
+
 extension TextEditor {
     public func firstResponder<V: Hashable>(id: V, firstResponder: Binding<V?>, resignableUserOperations: MbFirstResponder.TextEditor.ResignableUserOperations = .all, didBackViewLoaded: ((NSTextView) -> Void)? = nil) -> some View {
         self
