9f11b04f752736fc0ef9a7fbcb947b596918a56a
Denys Telezhkin <strangervir@gmail.com>
2023-04-26 15:08:47 +0300

    Add support for iOS SecureField.

--- Sources/MbSwiftUIFirstResponder/iOS/Interface.swift
+++ Sources/MbSwiftUIFirstResponder/iOS/Interface.swift
@@ -14,6 +14,13 @@ extension TextField {
     }
 }
 
+extension SecureField {
+    public func firstResponder<V: Hashable>(id: V, firstResponder: Binding<V?>, resignableUserOperations: MbFirstResponder.TextField.ResignableUserOperations = .all) -> some View {
+        self
+            .background(MbFRHackView<V, UITextField>(id: id, firstResponder: firstResponder, resignableUserOperations: resignableUserOperations))
+    }
+}
+
 extension TextEditor {
     public func firstResponder<V: Hashable>(id: V, firstResponder: Binding<V?>, resignableUserOperations: MbFirstResponder.TextEditor.ResignableUserOperations = .all) -> some View {
         self
